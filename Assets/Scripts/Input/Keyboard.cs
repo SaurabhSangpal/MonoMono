fe27885f9acbb61226ae66509adc9e9f09bf21fa
Saurabh Sangpal <saurabhsangpal@gmail.com>
2022-04-10 19:56:35 +0530

    Add Keyboard input class

---
+++ Assets/Scripts/Input/Keyboard.cs
@@ -0,0 +1,35 @@
+namespace Input
+{
+    internal class Keyboard : IController
+    {
+        public float GetHorizontalAxis()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public float GetVerticalAxis()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsJumpPressed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsDashPressed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsCrouchPressed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsAttackPressed()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

9bc7603a6dad5b1edca3fac400d0a560c3bc7cf0
Saurabh Sangpal <saurabhsangpal@gmail.com>
2022-04-11 21:40:10 +0530

    Add comments for IController

--- Assets/Scripts/Input/IController.cs
+++ Assets/Scripts/Input/IController.cs
@@ -1,13 +1,24 @@
 namespace Input
 {
+    /// <summary>
+    /// Represents a controller - can be a keyboard, gamepad, or touch screen.
+    /// </summary>
     internal interface IController
     {
+        /// <summary> Returns value between -1 to 1, -1 being left and 1 being right. </summary>
         float GetHorizontalAxis();
+        /// <summary> Returns value between -1 to 1, -1 being down and 1 being up. </summary>
         float GetVerticalAxis();
 
+        /// <summary> Returns true if the player has pressed the jump button in this frame. </summary>
         bool IsJumpPressed();
+        /// <summary> Returns true if the player has pressed the dash button in this frame. </summary>
         bool IsDashPressed();
+        /// <summary> Returns true if the player has pressed the crouch button in this frame.
+        /// </summary>
         bool IsCrouchPressed();
+        /// <summary> Returns true if the player has pressed the attack button in this frame.
+        /// </summary>
         bool IsAttackPressed();
     }
 }

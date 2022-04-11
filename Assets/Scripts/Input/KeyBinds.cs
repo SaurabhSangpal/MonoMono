dcd1f08eefa0dfc43f73af27424597ec009e57a1
Saurabh Sangpal <saurabhsangpal@gmail.com>
2022-04-11 21:57:38 +0530

    Add serializable KeyBinds class
    This class will be used to allow us to have a remappable key bind
    system.

---
+++ Assets/Scripts/Input/KeyBinds.cs
@@ -0,0 +1,20 @@
+namespace Input
+{
+    [System.Serializable]
+    public class KeyBinds
+    {
+        // If the input device is a joystick, we won't allow to rebind the input.
+        public bool isJoystick;
+
+        // These values will be null/empty when input device is a joystick.
+        public string left;
+        public string right;
+        public string up;
+        public string down;
+
+        public string jump;
+        public string dash;
+        public string crouch;
+        public string attack;
+    }
+}

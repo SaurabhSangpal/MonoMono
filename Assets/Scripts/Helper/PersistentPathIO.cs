10a229f1af2bfef99b2e7b29e959656d354f4adf
Saurabh Sangpal <saurabhsangpal@gmail.com>
2022-04-20 23:10:45 +0530

    Add comments to PersistentPathIO.cs

--- Assets/Scripts/Helper/PersistentPathIO.cs
+++ Assets/Scripts/Helper/PersistentPathIO.cs
@@ -3,13 +3,23 @@ using UnityEngine;
 
 namespace Helper
 {
+    /// <summary>
+    /// Perform IO operations on files in <see cref="Application.persistentdatapath"/>.
+    /// </summary>
     internal static class PersistentPathIO
     {
+        /// <summary>
+        /// Does the file exist at the given path in persistent path directory?
+        /// </summary>
         internal static bool Exists(string path)
         {
             return File.Exists(Application.persistentDataPath + "/" + path);
         }
 
+        /// <summary>
+        /// Load file at the given path in persistent path directory.
+        /// Throws exception if file does not exist.
+        /// </summary>
         internal static string LoadFile(string path)
         {
             if (!Exists(path))
@@ -18,6 +28,10 @@ namespace Helper
             return File.ReadAllText(Application.persistentDataPath + "/" + path);
         }
 
+        /// <summary>
+        /// Saves file at the given path with data in the persistent path directory.
+        /// If the file does not exist, it is created.
+        /// </summary>
         internal static void SaveFile(string path, string data)
         {
             var p = Application.persistentDataPath + "/" + path;
